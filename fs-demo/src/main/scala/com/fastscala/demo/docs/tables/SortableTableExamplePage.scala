64e74b5cdac2df93934c0ab74bb3cefeb6f00cb7
David Antunes <davidbranquinho@gmail.com>
2025-02-23 22:45:42 +0000

    Updates

--- fs-demo/src/main/scala/com/fastscala/demo/docs/tables/SortableTableExamplePage.scala
+++ fs-demo/src/main/scala/com/fastscala/demo/docs/tables/SortableTableExamplePage.scala
@@ -3,15 +3,34 @@ package com.fastscala.demo.docs.tables
 import com.fastscala.core.FSContext
 import com.fastscala.demo.docs.MultipleCodeExamples3Page
 import com.fastscala.components.bootstrap5.tables.*
+import com.fastscala.components.form7.{DefaultForm7, F7Field}
+import com.fastscala.components.form7.fields.text.F7StringField
 import com.fastscala.demo.testdata.{CountriesData, Country}
+import com.fastscala.scala_xml.js.JS
 
 class SortableTableExamplePage extends MultipleCodeExamples3Page() {
 
   override def pageTitle: String = "Sortable Table Example"
 
   override def renderAllCodeSamples()(implicit fsc: FSContext): Unit = {
+    import com.fastscala.demo.docs.forms.DefaultFSDemoBSForm7Renderers.*
+    import com.fastscala.components.bootstrap5.helpers.BSHelpers.*
     renderCodeSampleAndAutoClosePreviousOne("Source") {
-      new Table5Base
+      var countryNameFilter = ""
+      lazy val filterF: F7StringField = new F7StringField().label("Country name contains")
+        // Option A: filter on return:
+        //        .rw(countryNameFilter, v => countryNameFilter = v)
+        //        .addOnThisFieldChanged(field => {
+        //          table.rerenderTableAround()
+        //        })
+        // Option B:
+        .additionalAttrs(() => Seq(
+          "oninput" -> fsc.callback(JS("this.value"), value => {
+            countryNameFilter = value
+            table.rerenderTableAround()
+          }).cmd
+        ))
+      lazy val table = new Table5Base
         with Table5BaseBootrapSupport
         with Table5StandardColumns
         with Table5SeqSortableDataSource
@@ -37,8 +56,12 @@ class SortableTableExamplePage extends MultipleCodeExamples3Page() {
           case ColArea => _.sortBy(_.area)
         }
 
-        override def seqRowsSource: Seq[Country] = CountriesData.data
-      }.render()
+        override def seqRowsSource: Seq[Country] = CountriesData.data.filter(_.name.common.toLowerCase.contains(countryNameFilter.toLowerCase))
+      }
+      new DefaultForm7() with com.fastscala.components.form7.Form7WithSaveOnEnterHint {
+        override def rootField: F7Field = filterF
+      }.render().mb_2 ++
+        table.render()
     }
     closeCodeSample()
   }
